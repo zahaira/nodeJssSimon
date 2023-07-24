c143cfe93a17433d427997f523110ecf4c2527be
zahaira <zahairajaada@gmail.com>
2023-07-24 12:04:46 +0100

    edit somethings important for deploiment

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const sequelize = require('./src/db/sequelize')
 
 
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 app
 .use(favicon(__dirname + '/favicon.ico'))
-.use(morgan('dev'))
 .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req,res)=>{
+  res.json('hello me')
+})
 
 //piont de terminaiseon
 require('./src/routes/findAllPokemons')(app)
